53a5e7c8bcb739a8a3d05fa9f25dba9c91a925fb
sergeiam <sergei.m@gmail.com>
2019-05-27 22:27:11 +0300

    Shore wave WIP
    Added function to smooth the coastal mask based on the distance field.

--- source/shore_waves_generator/shore_waves_generator.cpp
+++ source/shore_waves_generator/shore_waves_generator.cpp
@@ -26,15 +26,19 @@ struct Vec3i
 	Vec3i(int _x, int _y, int _z) : x(_x), y(_y), z(_z) {}
 };
 
-template< class T > struct Image
+template< class T > class Image
 {
-	T* m_data;
+	T*	m_data;
+	int m_res;
 
-	Image() : m_data(new T[RES * RES]) { clear(); }
+public:
+
+	Image(int res) : m_data(new T[res * res]), m_res(res) { clear(); }
 	Image(const Image& rhs)
 	{
-		m_data = new T[RES * RES];
-		memcpy(m_data, rhs.m_data, RES * RES * sizeof(T));
+		m_res = rhs.m_res;
+		m_data = new T[num_texels()];
+		memcpy(m_data, rhs.m_data, size());
 	}
 	~Image()
 	{
@@ -42,11 +46,15 @@ template< class T > struct Image
 	}
 	void operator = (const Image& rhs)
 	{
-		memcpy(m_data, rhs.m_data, RES * RES * sizeof(T));
+		memcpy(m_data, rhs.m_data, size());
 	}
+	size_t resolution() const { return m_res; }
+	size_t size() const { return m_res * m_res * sizeof(T); }
+	size_t num_texels() const { return m_res * m_res; }
+
 	T& at(int x, int y)
 	{
-		return m_data[x + y * RES];
+		return m_data[x + y * m_res];
 	}
 	T& operator()(int x, int y)
 	{
@@ -62,20 +70,23 @@ template< class T > struct Image
 	}
 	void clear()
 	{
-		memset(m_data, 0, RES * RES * sizeof(T));
+		memset(m_data, 0, size());
 	}
 	template< class T2 > void convert_to(Image<T2>& image, T base)
 	{
-		for (int i = 0; i < RES * RES; ++i)
-			image[i] = T2(m_data[i] * base);
+		if (m_res == image.m_res)
+		{
+			for (size_t i = 0, n = num_texels(); i < n; ++i)
+				image[i] = T2(m_data[i] * base);
+		}
 	}
 	bool save(const char* filename)
 	{
 		FILE* fp = fopen(filename, "wb");
 		if (!fp) return false;
 
-		long bytes_written = fwrite(m_data, 1, RES * RES * sizeof(T), fp);
-		if (bytes_written != RES * RES * sizeof(T))
+		long bytes_written = fwrite(m_data, 1, size(), fp);
+		if (bytes_written != size())
 			return false;
 		fclose(fp);
 		return true;
@@ -85,16 +96,11 @@ template< class T > struct Image
 		FILE* fp = fopen(filename, "rb");
 		if (!fp) return false;
 
-		long bytes_read = fread(m_data, 1, RES * RES * sizeof(T), fp);
-		if (bytes_read != RES * RES * sizeof(T))
+		long bytes_read = fread(m_data, 1, size(), fp);
+		if (bytes_read != size())
 			return false;
 		fclose(fp);
 		return true;
-	}
-	void multiply_add(T mul, T add)
-	{
-		for (int i = 0; i < RES * RES; ++i)
-			m_data[i] = m_data[i] * mul + add;
 	}
 };
 
@@ -112,15 +118,17 @@ void compute_distance_and_normals(Image<u8>& mask, int radius, Image<u8>& distan
 	}
 	std::sort(sk.begin(), sk.end(), [](const Vec3i & a, const Vec3i & b) -> bool { return a.z < b.z; });
 
-	for (int i = 0; i < RES * RES; ++i)
+	for (int i = 0, n = mask.num_texels(); i < n; ++i)
 	{
 		distance[i] = mask[i] ? radius : 0;
 	}
 
+	const size_t res = mask.resolution();
+
 	const Vec3i* psk = &sk[0];
-	for (int y = 0; y < RES; ++y)
+	for (size_t y = 0; y < res; ++y)
 	{
-		for (int x = 0; x < RES; ++x)
+		for (size_t x = 0; x < res; ++x)
 		{
 			if (mask(x, y) == 0) continue;
 
@@ -162,59 +170,31 @@ void compute_distance_and_normals(Image<u8>& mask, int radius, Image<u8>& distan
 	}
 }
 
+void smooth_coast(Image<u8>& mask, int radius)
+{
+	compute_distance_and_normals(mask, radius, mask, nullptr, nullptr);
 
+	for (size_t i = 0, n = mask.num_texels(); i < n; ++i)
+		mask[i] = (mask[i] < radius) ? 255 : 0;
+
+	compute_distance_and_normals(mask, radius, mask, nullptr, nullptr);
+
+	for (int i = 0, n = mask.num_texels(); i < n; ++i)
+		mask[i] = (mask[i] < radius) ? 255 : 0;
+}
 
 int main()
 {
-	Image<u8> mask;
+	Image<u8> mask(2048), norm_x(2048), norm_y(2048);
 	if (!mask.load("map.raw"))
 	{
 		printf("ERROR: failed to load map.raw file!\n");
 		return 1;
 	}
 
-	Image<u8> norm_x, norm_y;
+	smooth_coast(mask, 16);
 
-	Image<float> df;
-	for (int i = 0; i < RES * RES; ++i)
-		df[i] = mask[i] ? SEARCH_DISTANCE : 0.0f;
-	
-// compute signed distance field
-#if 1
-	for (int y = 0; y < RES; ++y)
-	{
-		for (int x = 0; x < RES; ++x)
-		{
-			float v = df(x, y);
-			if (!v) continue;
-
-			if (x) v = min(v, df(x - 1, y) + 1.0f);
-			if( x && y ) v = min(v, df(x - 1, y - 1) + 1.414213f);
-			if( y ) v = min(v, df(x, y - 1) + 1.0f);
-			if (x + 1 < RES && y) v = min(v, df(x + 1, y - 1) + 1.414213f);
-
-			df(x, y) = v;
-			mask(x, y) = u8(v);
-		}
-	}
-	for (int y = RES-1; y >= 0; --y)
-	{
-		for (int x = RES-1; x >= 0; --x)
-		{
-			float v = df(x, y);
-			if (!v) continue;
-
-			if (x+1<RES) v = min(v, df(x + 1, y) + 1.0f);
-			if (x+1<RES && y+1<RES) v = min(v, df(x + 1, y + 1) + 1.414213f);
-			if (y+1<RES) v = min(v, df(x, y + 1) + 1.0f);
-			if (x && y+1<RES) v = min(v, df(x - 1, y + 1) + 1.414213f);
-
-			df(x, y) = v;
-			mask(x, y) = u8(v);
-		}
-	}
-	mask.save("sdf.raw");
-#endif
+	mask.save("map_smooth.raw");
 
 // compute brute-force distance field
 #if 1
@@ -224,183 +204,7 @@ int main()
 	norm_y.save("norm_y.raw");
 #endif
 
-// grow & dilate to smooth smaller coast features
-#if 1
-	Image<u8> grow(mask);
-	for (int i = 0; i < RES * RES; ++i)
-		grow[i] = (grow[i] < 8) ? 255 : 0;
-	grow.save("grow.raw");
-	compute_distance_and_normals(grow, 16, grow, nullptr, nullptr);
 
-	for (int i = 0; i < RES * RES; ++i)
-		grow[i] = (grow[i] < 12) ? 255 : 0;
-	grow.save("shrink.raw");
-
-#endif
-
-
-
-// Outline
-#if 1
-	Image<u8> outline;
-	for (int y = 0; y < RES-1; ++y)
-	{
-		for (int x = 0; x < RES-1; ++x)
-		{
-			u8 quad[4] = { mask(x, y), mask(x + 1,y), mask(x,y + 1), mask(x + 1,y + 1) };
-			u8 vmin = min(min(quad[0], quad[1]), min(quad[2], quad[3]));
-			u8 vmax = max(max(quad[0], quad[1]), max(quad[2], quad[3]));
-
-			outline(x, y) = (vmin == 0 && vmax) ? 255 : 0;
-		}
-	}
-	outline.save("outline.raw");
-#endif
-
-// Clean-up outline from excessive pixels
-#if 1
-	for (int pass = 0; pass < 2; ++pass)
-	{
-		for (int y = 0; y < RES; ++y)
-		{
-			for (int x = 0; x < RES; ++x)
-			{
-				int ofs[8][2] = { {-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1} };
-				u8 mask = 0;
-				for (int i = 0; i < 8; ++i, mask <<= 1)
-				{
-					int nx = x + ofs[i][0], ny = y + ofs[i][1];
-					if (!inside(nx,ny)) continue;
-					if (outline(nx, ny)) mask |= 1;
-				}
-
-				if (mask == 0xFF)
-				{
-					outline(x, y) = 0;
-				}
-				else
-				{
-					while (mask & 128) mask = (mask >> 1) | ((mask & 1) << 7);  // rotate right
-					while (mask && (mask & 1) == 0) mask >>= 1;
-
-					bool continuous = true;
-					for (; mask; mask >>= 1)
-					{
-						if ((mask & 1) == 0) continuous = false;
-					}
-					if (continuous)
-					{
-						outline(x, y) = 0;
-					}
-				}
-			}
-		}
-	}
-	outline.save("outline.raw");
-#endif
-
-// walk the outline
-#if 1
-	Image<u8> used;
-	Image<float> outlinef;
-
-	for (int y = 0; y < RES; ++y)
-	{
-		for (int x = 0; x < RES; ++x)
-		{
-			if (outline(x, y) == 0 || used(x, y)) continue;
-
-			outline(x, y) = 0;
-			used(x, y) = 1;
-
-			int xc = x, yc = y;
-
-			for (int value = 1;; ++value)
-			{
-				int ofs[4][2] = { {-1,0},{0,1},{1,0},{0,-1} };
-				int xn = -1, yn = -1;
-
-				for (int i = 0; i < 4; ++i)
-				{
-					int nx = xc + ofs[i][0], ny = yc + ofs[i][1];
-
-					if ( ! inside(nx,ny )) continue;
-
-					if (used(nx, ny)) continue;
-					if (outline(nx, ny) == 0) continue;
-
-					outline(nx, ny) = u8(value & 0xFF);
-					outlinef(nx, ny) = value;
-					used(nx, ny) = 1;
-
-					xn = nx;
-					yn = ny;
-				}
-				if (xn == -1) break;
-
-				xc = xn;
-				yc = yn;
-			}
-		}
-	}
-	outline.save("uv.raw");
-#endif
-
-// blur outline
-#if 1
-	mask.save("debug_mask.raw");
-	used.save("debug_used.raw");
-	for (int pass = 0; pass < 1; ++pass)
-	{
-		Image<u8> dest_outline;
-		Image<u8> dest_used;
-		Image<float> dest_outlinef;
-
-		for (int y = 0; y < RES; ++y)
-		{
-			for (int x = 0; x < RES; ++x)
-			{
-				if (mask(x, y) == 0 || mask(x, y) == SEARCH_DISTANCE || used(x, y)) continue;
-
-				const int ofs[8][2] = { {-1,0},{0,1},{1,0},{0,-1}, {1,1}, {-1,1}, {1,-1}, {-1,-1} };
-				const float w[8] = { 1.0f, 1.0f, 1.0f, 1.0f, 0.77f, 0.77f, 0.77f, 0.77f };
-
-				int sum = 0, count = 0;
-				float wsum = 0.0f, wweight = 0.0f;
-
-				for (int i = 0; i < 8; ++i)
-				{
-					int nx = x + ofs[i][0];
-					int ny = y + ofs[i][1];
-
-					if (!inside(nx,ny)) continue;
-
-					if (!used(nx, ny)) continue;
-
-					sum += outline(nx, ny);
-					count++;
-
-					wsum += outlinef(nx, ny);
-					wweight += w[i];
-				}
-				if (count)
-				{
-					dest_outline(x, y) = sum / count;
-					dest_outlinef(x, y) = wsum / wweight;
-					dest_used(x, y) = 1;
-				}
-			}
-		}
-		outline = dest_outline;
-		outlinef = dest_outlinef;
-		used = dest_used;
-	}
-	outline.save("uv_blurred.raw");
-
-	Image<u8> ofsave;
-	outlinef.convert_to(ofsave, 1.0f);
-	ofsave.save("uvf_blurred.raw");
-#endif
 
 	return 0;
 }
